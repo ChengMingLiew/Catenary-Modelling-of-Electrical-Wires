d9776f10bcb75c710cdba87e6741fab5b7f4a549
ChengMingLiew <chengmingliew@LAPTOP-1A92TAQQ.>
2025-04-11 14:55:37 +1000

    slight changes

--- lidar_functions/optimise_functions.py
+++ lidar_functions/optimise_functions.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+# Finds the parameters x_0 and y_0 for the Catenary Equation
 def find_param(points):
 
     x_naught = points[np.where(points[:, 1] == points[:, 1].min()), :][0][0][0]
@@ -10,6 +11,7 @@ def find_param(points):
 def catenary(x, y0, c, x0):
     return y0 + c * (np.cosh((x - x0) / c) - 1)
 
+# Mean Squared Error Loss function with a specific Catenary Model
 def loss(c, points, x0, y0):
     y_pred = catenary(points[:, 0], y0, c, x0)
     return np.mean((points[:, 1] - y_pred) ** 2)
